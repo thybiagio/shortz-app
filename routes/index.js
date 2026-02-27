c7b0f9fef5e634b4cc846eee3a55f451edc9e816
Thyago Biagio <thybiagio@gmail.com>
2026-02-26 22:13:59 -0300

    A3_2

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bem vindo!' });
+  console.log("app inicializado e respondendo")
 });
+
+// /register - rota
+// 'register' - register.ejs
+router.get('/register', function(req, res, next){ 
+  res.render('register', { title : 'Criar conta'});
+})
 
 module.exports = router;
